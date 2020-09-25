4b76816f595713c648a312ec82a402bae998cf57
“Chris <hh0975832701@gmail.com>
2020-09-25 13:53:40 +0800

    Initial Commit

---
+++ 147/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  147
+//
+//  Created by User24 on 2020/9/25.
+//  Copyright © 2020 yuen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
